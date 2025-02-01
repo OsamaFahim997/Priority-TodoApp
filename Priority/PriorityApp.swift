bb7a57fa83da1296ace58b301ef30e44645c036f
Osama Fahim <osama.fahim@solera.com>
2025-02-01 18:28:12 +0100

    Initial Commit

---
+++ Priority/PriorityApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriorityApp.swift
+//  Priority
+//
+//  Created by Osama Fahim on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PriorityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
